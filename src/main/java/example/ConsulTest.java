4b32c23716d5d84b441e539c0f331a78582efed2
Eduard Coll <ecollvila@gmail.com>
2016-06-19 12:51:16 +0200

    Add consul configuration client.
    Signed-off-by: Eduard Coll <ecollvila@gmail.com>

--- src/main/java/example/ConsulTest.java
+++ src/main/java/example/ConsulTest.java
@@ -1,23 +1,34 @@
 package example;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
+@RefreshScope
 public class ConsulTest {
-
-  @Cacheable
+  
+  @Value("${consul-client-v0.properties.consul.client.message:Hello World}")
+  private String message;
+  /**
+   * 
+   * @return value of message property
+   */
+//  @Cacheable
   @RequestMapping("/")
   public String home() {
-    return "Hello World";
+    return message;
   }
-
+  /**
+   * Main method of the application
+   * @param args type of String[]
+   */
   public static void main(String[] args) {
     SpringApplication.run(ConsulTest.class, args);
   }
